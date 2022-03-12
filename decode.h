1009e63eb1da468c5c98319b86958da4d6042fa2
Stepan <stepan-pl@protonmail.com>
2022-03-12 22:20:22 +0300

    commit

---
+++ decode.h
@@ -0,0 +1,17 @@
+/*string DeVIGENERE(string messange, string key)
+{
+    int position = 1;
+    string decodirovka;
+    vector<int> skey;
+    for (auto i : key)
+    {
+        skey.push_back(i - 128);
+    }
+    for (auto i : messange)
+    {
+        if (position > skey.size()) position = 1;
+        if (i - skey[position] >= 128) decodirovka += i - skey[position] + 6;
+        else decodirovka += 159 - skey[position] + (i - 128) + 2;
+    }
+    return decodirovka;
+}*/
